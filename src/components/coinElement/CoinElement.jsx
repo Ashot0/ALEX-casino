233d398c852ba9ba2805a1eecab975123ae48070
Ashot0 <illia.holovan@gmail.com>
2024-01-10 21:18:01 +0200

    BugFix

--- src/components/coinElement/CoinElement.jsx
+++ src/components/coinElement/CoinElement.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
 import './coin-element.scss';
 import coinImg from './coin.png';
 
-const Coin = ({ id, x, y, onRemove }) => {
-	const [clicked, setClicked] = useState(false);
+const Coin = ({ id, x, y, mouseX, mouseY, onRemove }) => {
+	const [falling, setFalling] = useState(true);
+  const [clicked, setClicked] = useState(false);
 
 	const handleClick = () => {
 		setClicked(true);
 		onRemove(id);
 	};
 
-	const styles = useSpring({
-		from: { opacity: 0, transform: `translate(${x}px, -50px)` },
+	useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFalling(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+
+	const fallingStyles = useSpring({
+			from: { opacity: 0, transform: `translate(${x}px, -50px)` },
 		to: {
 			opacity: clicked ? 0 : 1,
 			transform: `translate(${x}px, ${y}px)`,
 		},
 		config: { tension: 100, friction: 10 },
-	});
+  });
 
+	const attractionStyles = useSpring({
+		from: { opacity: clicked ? 0 : 1,
+			transform: `translate(${x}px, ${y}px)`, },
+    to: { opacity: clicked ? 0 : 1, transform: `translate(${mouseX}px, ${mouseY}px)` },
+    config: { tension: 0.1, friction: 30 },
+  });
 	return (
-		<animated.div className="coin" style={styles} onClick={handleClick}>
+		<animated.div className="coin" style={falling ? fallingStyles : attractionStyles} onClick={handleClick}>
 			{clicked ? null : (
-				<img className="coin__image" src={coinImg} alt="" />
+				<img className="coin__image"  src={coinImg} alt="" />
 			)}
 		</animated.div>
 	);
--- src/components/coinPage/CoinPage.jsx
+++ src/components/coinPage/CoinPage.jsx
@@ -5,38 +5,48 @@ import './coin-page.scss';
 
 const CoinPage = () => {
 	const [coins, setCoins] = useState([]);
-
+	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	
 	useEffect(() => {
 		const interval = setInterval(() => {
-			const newCoin = {
-				id: Date.now(),
-				x: Math.random() * window.innerWidth,
-				y: Math.random() * window.innerHeight,
-			};
-
-			setCoins((prevCoins) => [...prevCoins, newCoin]);
+			if (coins.length < 100) {
+				console.log(`${coins.length}/100`);
+        const newCoin = {
+          id: Date.now(),
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        };
+        setCoins((prevCoins) => [...prevCoins, newCoin]);
+      } else {
+        clearInterval(interval);
+      }
 		}, 1000);
+			return () => clearInterval(interval);
+	}, [coins.length]);
 
-		return () => clearInterval(interval);
-	}, []);
+	const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
 
 	const handleRemoveCoin = (id) => {
 		setCoins((prevCoins) => prevCoins.filter((coin) => coin.id !== id));
 	};
 
-	return (
-		<div className="coin-page">
-			{coins.map((coin) => (
-				<Coin
-					key={coin.id}
-					id={coin.id}
-					x={coin.x}
-					y={coin.y}
-					onRemove={handleRemoveCoin}
-				/>
-			))}
-		</div>
-	);
+	 return (
+    <div className="coin-page" onMouseMove={handleMouseMove}>
+      {coins.map((coin) => (
+        <Coin
+          key={coin.id}
+          id={coin.id}
+          x={coin.x}
+          y={coin.y}
+          mouseX={mousePosition.x}
+          mouseY={mousePosition.y}
+          onRemove={handleRemoveCoin}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CoinPage;
